a546fff87b82eb826130edd0fad8e307c704add5
nariman.hasanpourarba <nariman.hasanpourarba@student.vinci.be>
2022-12-06 13:22:25 +0100

    first commit , need to link with a DB

---
+++ src/main/java/ipl/pfe/group04/kickerDaVinci/KickerDaVinciApplication.java
@@ -0,0 +1,13 @@
+package ipl.pfe.group04.kickerDaVinci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickerDaVinciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickerDaVinciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ipl/pfe/group04/kickerDaVinci/KickerDaVinciApplicationTests.java
@@ -0,0 +1,13 @@
+package ipl.pfe.group04.kickerDaVinci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickerDaVinciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

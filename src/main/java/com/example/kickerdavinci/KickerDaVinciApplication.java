377c4ded76a3a82975408bbdc43137b353cd5cee
Traqnar <nariman.hasanpourarba@student.vinci.be>
2022-12-12 19:27:42 +0100

    Update KickerDaVinciApplication.java

--- src/main/java/com/example/kickerdavinci/KickerDaVinciApplication.java
+++ src/main/java/com/example/kickerdavinci/KickerDaVinciApplication.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+
 @SpringBootApplication
 public class KickerDaVinciApplication {
 
